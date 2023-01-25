b2e58bafa2d2fe991f644a1408a185d746314598
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-25 14:43:42 +0200

    add login user route with controller

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -41,3 +41,37 @@ export const createUser = async (req: Request, res: Response) => {
     console.error(error);
   }
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    if (!process.env.JWT_SECRET) throw "No JWT_SECRET found on .env file";
+
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("Invalid email or password");
+    }
+
+    const { password, ...remainingData } = user.toJSON();
+
+    const passwordMatch = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordMatch) {
+      return res.status(404).send("Invalid email or password");
+    }
+
+    const token = jwt.sign(
+      { id: user.id, isAdmin: false },
+      process.env.JWT_SECRET,
+      { expiresIn: "1 day" }
+    );
+
+    return res
+      .cookie("session_token", token, {
+        httpOnly: true,
+      })
+      .status(201)
+      .json(remainingData);
+  } catch (error) {
+    res.status(405).send(error);
+    console.error(error);
+  }
+};
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/authController";
+import { createUser, loginUser } from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.post("/login", loginUser);
 
 export default router;
