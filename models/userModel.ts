0614bd98612751bea9cf6c218fa0e66a22a69db6
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-25 12:08:13 +0200

    add user model

---
+++ models/userModel.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  rentMovies: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      time: {
+        type: Number,
+        default: 12,
+      },
+    },
+  ],
+});
+
+export default mongoose.model("user", userSchema);
